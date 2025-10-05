a6caaaae1ed5fc343204f2e75b871b55cea7b2c3
Ixalon <ixalon@gmail.com>
2025-10-05 16:56:49 -0500

    Update spicychat_analytics.py

--- spicychat_analytics.py
+++ spicychat_analytics.py
@@ -23,6 +23,7 @@ from urllib.parse import urlparse
 import hashlib
 import pytz
 
+
 # ------------------ Config ------------------
 BASE_DIR = Path(__file__).resolve().parent
 DATA_DIR = BASE_DIR / "data"
